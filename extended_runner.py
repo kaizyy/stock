4d6e48bfa51500046fe633cdba4765df2e4b819c
kaizyy <61597655+kaizyy@users.noreply.github.com>
2026-08-25 20:20:45 +0200

    Bump dashboard scripts for dedicated analytics view

--- extended_runner.py
+++ extended_runner.py
@@ -40,7 +40,7 @@ class ExtendedHandler(app_runner.AppHandler):
             content = (server.PUBLIC_DIR / "index.html").read_text(encoding="utf-8")
             content = content.replace(
                 "</body>",
-                '<script src="/settings.js?v=20260825-8"></script><script src="/features.js?v=20260825-8"></script><script src="/features_optional_fix.js?v=20260825-8"></script><script src="/role_dashboard.js?v=20260825-8"></script><script src="/analytics_dashboard.js?v=20260825-8"></script><script src="/inventory_intelligence.js?v=20260825-8"></script><script src="/barcode_scanner_fallback.js?v=20260825-8"></script><script src="/dynamic_navigation.js?v=20260825-8"></script><script src="/crm_orders.js?v=20260825-8"></script><script src="/order_delete_ui.js?v=20260825-8"></script><script src="/warehouse_ops.js?v=20260825-8"></script></body>'
+                '<script src="/settings.js?v=20260825-9"></script><script src="/features.js?v=20260825-9"></script><script src="/features_optional_fix.js?v=20260825-9"></script><script src="/role_dashboard.js?v=20260825-9"></script><script src="/analytics_dashboard.js?v=20260825-9"></script><script src="/inventory_intelligence.js?v=20260825-9"></script><script src="/barcode_scanner_fallback.js?v=20260825-9"></script><script src="/dynamic_navigation.js?v=20260825-9"></script><script src="/crm_orders.js?v=20260825-9"></script><script src="/order_delete_ui.js?v=20260825-9"></script><script src="/warehouse_ops.js?v=20260825-9"></script></body>'
             )
             self.send_html(200, content)
             return
